616f4bc14e5d7d7e286bb5896e3c6c1d4185fb3a
Dongho Gang <gongdongho12@gmail.com>
2022-05-15 00:43:22 +0900

    code: 주석항목 제거

--- src/main/kotlin/com/dongholab/kafka/model/ApiMessage.kt
+++ src/main/kotlin/com/dongholab/kafka/model/ApiMessage.kt
@@ -1,17 +1,7 @@
 package com.dongholab.kafka.model
 
-
-//@JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class ApiMessage(
-//    @field:JsonProperty("host")
     val host: String = "",
-//    @field:JsonProperty("method")
     val method: String = "",
-//    @field:JsonProperty("type")
     val type: String = ""
 )
-//data class ApiMessage @ConstructorProperties("host", "method", "type") constructor(
-//    val host: String = "",
-//    val method: String = "",
-//    val type: String = "",
-//)
